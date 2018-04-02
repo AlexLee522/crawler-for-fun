38ab157da7f777d8319668892410cf4d8695d85f
957 <32644012+AlexLee522@users.noreply.github.com>
2018-04-02 22:36:05 +0800

    Create qqmusic.py

---
+++ qqmusic.py
@@ -0,0 +1,25 @@
+import requests
+
+
+# 获取输入url中的songmid
+url0 = input('输入目标url:')
+songmid = url0[28:-5]
+
+# 获取vkey
+url1 = 'https://c.y.qq.com/base/fcgi-bin/fcg_music_express_mobile3.fcg?g_tk=5381&loginUin=0&hostUin=0&format=json&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&cid=205361747&songmid=' + \
+    songmid + '&filename=C400' + songmid + '.m4a&guid=6113762648'
+data = requests.get(url1).text.split(',')
+vkey = data[7][8:-5]
+
+# 获取qq音乐下载url
+url2 = 'http://dl.stream.qqmusic.qq.com/C400' + songmid + \
+    '.m4a?' + vkey + '&guid=6113762648&uin=0&fromtag=66'
+
+# 下载音乐
+headers = {
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.62 Safari/537.36', 'Host': 'dl.stream.qqmusic.qq.com'}
+parameters = {'vkey': vkey}
+data = requests.get(url2, headers=headers, params=parameters).content
+file = open('qqmusic.m4a', 'wb')
+file.write(data)
+file.close()
